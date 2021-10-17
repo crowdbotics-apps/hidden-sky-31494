d0230c6693ae8cb7fb84bd391aabab54a72737fb
Crowdbotics <team@crowdbotics.com>
2021-10-17 19:45:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hidden_sky_31494",
+  url: "https://hidden_sky_31494.botics.co",
+  api: "https://hidden_sky_31494.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
